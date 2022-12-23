89622c57bdf1f91b36f6ab9a80503aef57011e43
Niharika Bommireddy <niharikabommireddy21@gmail.com>
2022-12-23 13:15:07 +0530

    Vehicle code Constructor added modified version

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -7,6 +7,12 @@ public class Boat implements WaterVehicle {
     {
 
     }
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
     @Override
     public String getVehicleName() {
         return name;

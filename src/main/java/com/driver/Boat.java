5c18b2a391e0a49a42eebbe151f6ff276a7f981f
Niharika Bommireddy <niharikabommireddy21@gmail.com>
2022-12-23 13:59:12 +0530

    restart code1

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -5,11 +5,7 @@ public class Boat implements WaterVehicle {
     int capacity;
 
 
-    public Boat(String name,int capacity)
-    {
-        this.name =name;
-        this.capacity = capacity;
-    }
+
 
     @Override
     public String getVehicleName() {
